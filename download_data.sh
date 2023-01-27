6604c70c8636fd4de1f4f6de2472f64a31219209
Nikola Ljubešić <nljubesi@gmail.com>
2023-01-27 07:06:48 +0100

    First baseline

---
+++ download_data.sh
@@ -0,0 +1,8 @@
+rm -rf data/
+mkdir data
+cd data
+wget https://www.clarin.si/repository/xmlui/bitstream/handle/11356/1461/SETimes.HBS.zip
+unzip SETimes.HBS.zip
+wget https://www.clarin.si/repository/xmlui/bitstream/handle/11356/1482/Twitter-HBS.zip
+unzip Twitter-HBS.zip
+cd ..
